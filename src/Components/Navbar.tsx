c9043e23ddcae5d5790eafbe46f5ffeb583d02fd
Majd Qumseya <qumseya.majd@gmail.com>
2023-03-04 13:50:37 +0200

    Added styling and logout feature

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,11 +1,36 @@
-
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
 export const Navbar = () => {
+  const [user] = useAuthState(auth);
+
+  const signUserOut = async () => {
+    await signOut(auth);
+  };
+
   return (
-    <div>
+    <div className="navbar">
+      {user && (
+        <div className="user-info">
+          <img
+            src={user?.photoURL || ""}
+            alt="user photo"
+            height={50}
+            width={50}
+          />
+          <p>{user?.displayName}</p>
+          <button className="logout" onClick={signUserOut}>
+            Log Out
+          </button>
+        </div>
+      )}
+
+      <div className="links">
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/Pages/login.tsx
+++ src/Pages/login.tsx
@@ -1,10 +1,13 @@
 import { auth, provider } from "../config/firebase";
 import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
+  const navigate = useNavigate();
   const signInWithGoogle = async () => {
     const signInResults = await signInWithPopup(auth, provider);
-    console.log(signInResults)
+    console.log(signInResults);
+    navigate("/");
   };
   return (
     <div>
