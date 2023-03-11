98256f877318eae8e3e1b321ee81402c311dccb7
Majd Qumseya <qumseya.majd@gmail.com>
2023-03-11 11:44:30 +0200

    added form to create posts, hooked it up with firestorm and set up config/pemissions

--- src/Pages/create-post/CreatePostForm.tsx
+++ src/Pages/create-post/CreatePostForm.tsx
@@ -1,33 +1,56 @@
 import { useForm } from "react-hook-form";
-import { object, string, number, ref } from "yup";
+import { object, string } from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-
-let postSchema = object({
-  title: string().required("Blog title is required"),
-  description: string().required("Blog description is required"),
-  username: string().required("Please enter a valid username"),
-  id: string().min(4).max(20).required("id is required."),
-});
-
-const {
-  register,
-  handleSubmit,
-  formState: { errors },
-} = useForm({
-  resolver: yupResolver(postSchema),
-});
-
-const onSubmit = async (d) => {
-  const post = await postSchema.validate(d);
-  console.log({ post });
-};
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import {useNavigate} from 'react-router-dom'
+ 
+interface CreatePostData {
+  title: string;
+  description: string;
+}
 
 export const CreatePostForm = () => {
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  const postSchema = object().shape({
+    title: string().required("Title is required."),
+    description: string().required("Description is required."),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreatePostData>({
+    resolver: yupResolver(postSchema),
+  });
+
+  const postsRef = collection(db, "posts");
+
+  const onCreatePost = async (data: CreatePostData) => {
+    try {
+      const docRef = await addDoc(postsRef, {
+        ...data,
+        username: user?.displayName,
+        userId: user?.uid,
+      });
+      console.log("Document written with ID: ", docRef.id);
+      navigate("/");
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} method="post">
-     <input type="text" placeholder="Title..." {...register("title")} />
-     <input type="text" placeholder="Description..."{...register("desscription")} />
-     <input type="submit"  />
+    <form onSubmit={handleSubmit(onCreatePost)} method="post">
+      <input type="text" placeholder="Title..." {...register("title")} />
+      <p style={{ color: "red" }}>{errors.title?.message}</p>
+      <textarea placeholder="Description..." {...register("description")} />
+      <p style={{ color: "red" }}>{errors.description?.message}</p>
+      <input type="submit" />
     </form>
   );
 };
