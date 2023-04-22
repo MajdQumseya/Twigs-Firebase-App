2cf3f9c8b23f90055a84663b232aed16ae576f10
Majd Qumseya <qumseya.majd@gmail.com>
2023-04-22 13:01:46 +0300

    finished with the firebase integrated like system, added basic styling for home page

--- src/Pages/home/Post.tsx
+++ src/Pages/home/Post.tsx
@@ -1,4 +1,4 @@
-import { addDoc, getDocs, collection, query, where } from "firebase/firestore";
+import { addDoc, getDocs, deleteDoc, collection, query, where, doc } from "firebase/firestore";
 import { Post as IPost } from "./home";
 import { auth, db } from "../../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -8,38 +8,65 @@ interface PostProps {
   post: IPost;
 }
 
+interface Like {
+  likeId: string;
+  userId: string;
+}
+
 export const Post = (props: PostProps) => {
   const { post } = props;
   const [user] = useAuthState(auth);
-  const [likeAmount, setLikeAmount] = useState<number | null>(null)
+  const [likes, setLikes] = useState<Like[] | null>(null);
 
   const likesRef = collection(db, "likes");
+  const likesDoc = query(likesRef, where("postId", "==", post.id));
 
-  const getLikes = async() => {
-    const data = await getDocs(likesDoc)
-    setLikeAmount(data.size)
-  }
-
-  const likesDoc = query(likesRef, where("postId", "==", post.id))
-  
+  const getLikes = async () => {
+    const data = await getDocs(likesDoc);
+    setLikes(data.docs.map((doc) => ({ userId: doc.data().userId, likeId: doc.id })));
+  };
 
   const addLike = async () => {
     try {
-       await addDoc(likesRef, {userId: user?.uid, postId: post.id});
-      
-     
+      const newDoc = await addDoc(likesRef, { userId: user?.uid, postId: post.id });
+      if (user) {
+        setLikes((prev) =>
+          prev ? [...prev, { userId: user?.uid, likeId: newDoc.id }] : [{ userId: user?.uid, likeId: newDoc.id }]
+        );
+      }
     } catch (e) {
       console.error("Error adding like: ", e);
     }
   };
 
-  useEffect(() => {
-    getLikes()
-  }, []);
+  const removeLike = async () => {
+    try {
+      const likeToDeleteQuery = query(likesRef, where("postId", "==", post.id), where("userId", "==", user?.uid));
+      const likeToDeleteData = await getDocs(likeToDeleteQuery)
+      const likeId = likeToDeleteData.docs[0].id;
+      const likeToDelete = doc(db, 'likes', likeId)
+     
 
+      await deleteDoc(likeToDelete);
+
+      if (user) {
+        setLikes((prev) => prev && prev.filter((like) => like.likeId !== likeId));
+      }
+      
+    } catch (e) {
+      console.error("Error adding like: ", e);
+    }
+  };
+
+  const hasUserLiked = likes?.find((like) => like.userId === user?.uid);
+
+  useEffect(() => {
+    getLikes();
+  }, []);
 
   return (
     <div>
+      <hr></hr>
       <div className="title">
         <h1>{post.title}</h1>
       </div>
@@ -48,11 +75,11 @@ export const Post = (props: PostProps) => {
       </div>
       <div className="footer">
         <p>@{post.username}</p>
-        <button onClick={addLike}> &#128077; </button>
-        {likeAmount && <p>Likes: {likeAmount}</p>}
+        <button onClick={hasUserLiked ? removeLike : addLike}>
+          {hasUserLiked ? <>&#128078; </> : <>&#128077;</>}{" "}
+        </button>
+        {likes && <p>Likes: {likes?.length}</p>}
       </div>
     </div>
   );
 };
-
-
